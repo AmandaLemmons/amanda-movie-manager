68f2948f97d2227d6a8bc0ef172cd66f0aa31b01
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-08-04 21:36:12 -0500

    add main page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,9 @@
 class Movie < ActiveRecord::Base
    validates :title, length: {in: 1..50 }, presence: true
    validates :format, presence: true
-   validates :length, presence: true
+   validates_numericality_of :length, less_than_or_equal_to: 500, greater_than_or_equal_to: 0, presence: true
    validates :rating, presence: true
-   validates_numericality_of :release_year, less_than_or_equal_to: 2100, greater_than_or_equal_to: 1800, presence: true 
+   validates_numericality_of :release_year, less_than_or_equal_to: 2100, greater_than_or_equal_to: 1800, presence: true
 
 
 end
