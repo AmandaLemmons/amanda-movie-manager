d5014aa2dfe12d83102561dbd407d325a3b41d87
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-08-20 16:09:28 -0500

    add bug - title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-   validates :title, length: {in: 1..50 }, presence: true
+  # validates :title, length: {in: 1..50 }, presence: true
    validates :format, presence: true
    validates_numericality_of :length, less_than_or_equal_to: 500, greater_than_or_equal_to: 0, presence: true
    validates :rating, presence: true
